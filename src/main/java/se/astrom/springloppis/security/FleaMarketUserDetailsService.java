ae2ca1ff509c194e11d6e51b434834fc7d46be6e
johanastrom <johan.astrom89@gmail.com>
2022-01-25 13:49:27 +0100

    Added Security settings

--- src/main/java/se/astrom/springloppis/security/FleaMarketUserDetailsService.java
+++ src/main/java/se/astrom/springloppis/security/FleaMarketUserDetailsService.java
@@ -2,9 +2,12 @@ package se.astrom.springloppis.security;
 
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 import se.astrom.springloppis.entity.UserEntity;
 import se.astrom.springloppis.repository.UserRepository;
 
+@Service
 public class FleaMarketUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
@@ -15,11 +18,11 @@ public class FleaMarketUserDetailsService implements UserDetailsService {
     }
 
     @Override
-    public UserDetails loudUsername(String username) {
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         UserEntity userEntity = userRepository.findByUsername(username);
         if (userEntity == null){
-            System.out.println("Can't find user with username " + username);
+            throw new UsernameNotFoundException("Can't find user with username " + username);
         }
-        return new
+        return new FleaMarketUserPrincipal(userEntity);
     }
 }
--- src/main/java/se/astrom/springloppis/security/SecurityConfig.java
+++ src/main/java/se/astrom/springloppis/security/SecurityConfig.java
@@ -1,17 +1,42 @@
 package se.astrom.springloppis.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private FleaMarketUserDetailsService userDetailsService;
+
+    public SecurityConfig(FleaMarketUserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+        var provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(new BCryptPasswordEncoder());
+        return provider;
+    }
+
     @Override
-    protected void configure(HttpSecurity httpSecurity){
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.authenticationProvider(authenticationProvider());
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/", "home").permitAll()
                 .anyRequest().authenticated()
@@ -22,7 +47,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logout()
                 .invalidateHttpSession(true)
                 .clearAuthentication(true)
-                .permitAll()
+                .permitAll();
 
     }
 }
